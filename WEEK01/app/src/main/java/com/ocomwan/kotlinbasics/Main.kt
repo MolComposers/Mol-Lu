9af9b534fe35849a539d80d666b6a6e0b129b89e
b1urrrr <b1urrr@naver.com>
2023-11-16 00:20:06 +0900

    [ADD/#1] 코틀린 max, min 함수 실습

--- WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
+++ WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
@@ -1,14 +1,14 @@
 package com.ocomwan.kotlinbasics
 
+import kotlin.math.max
+import kotlin.math.min
+
 const val num = 20
 
 fun main() {
-    var name = "hello"
-    print(name.uppercase())
-    print(name.lowercase())
-    print(name[0])
+    val i = 10
+    val j = 20
 
-    name = "채연"
-    print("제 이름은 ${name}입니다.")
-    print("제 이름은 ${name + 10}입니다.")
+    print(max(i, j))
+    print(min(i, j))
 }
