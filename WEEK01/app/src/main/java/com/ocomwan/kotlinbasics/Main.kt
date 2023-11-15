e8e2132a4378867cc94fd25e74227931fbddd116
b1urrrr <b1urrr@naver.com>
2023-11-16 00:04:12 +0900

    [ADD/#1] 컴파일 타임 상수 실습

--- WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
+++ WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
@@ -1,10 +1,11 @@
+package com.ocomwan.kotlinbasics
+
+const val num = 20
+
 fun main() {
     var i = 10
     var name = "채연"
     var point = 3.3
 
     i = 20
-
-    val num = 20
-    // num = 30
 }
