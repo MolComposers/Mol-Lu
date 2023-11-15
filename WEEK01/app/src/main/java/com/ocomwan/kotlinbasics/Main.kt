1c54118f0cfec45c5d6ccf0e94c7df2d894dbbb8
b1urrrr <b1urrr@naver.com>
2023-11-16 00:16:09 +0900

    [ADD/#1] 코틀린 String 실습

--- WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
+++ WEEK01/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
@@ -3,15 +3,12 @@ package com.ocomwan.kotlinbasics
 const val num = 20
 
 fun main() {
-    var i = 10
-    var l = 20L
+    var name = "hello"
+    print(name.uppercase())
+    print(name.lowercase())
+    print(name[0])
 
-    l = i.toLong()
-    i = l.toInt()
-
-    var name = ""
-    name = i.toString()
-
-    var name2 = "10"
-    i = name2.toInt()
+    name = "채연"
+    print("제 이름은 ${name}입니다.")
+    print("제 이름은 ${name + 10}입니다.")
 }
