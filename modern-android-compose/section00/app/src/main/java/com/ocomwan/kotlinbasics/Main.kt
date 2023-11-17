ddbe694be50b5ec0e04a587ee101f650b3868f5a
b1urrrr <b1urrr@naver.com>
2023-11-17 23:41:02 +0900

    [RENAME/#1] section00 디렉토리명 변경

--- modern-android-compose/section00/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
+++ modern-android-compose/section00/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
@@ -21,4 +21,21 @@ fun main() {
     println(numberInput)
     val stringInput = reader.next()
     println(stringInput)
+
+    // 조건문
+    val i = 5
+    if (i > 10) {
+        print("10보다 크다")
+    } else if (i > 5) {
+        print("5보다 크다")
+    } else {
+        print("")
+    }
+
+    val result = when {
+        i > 10 -> "10보다 크다"
+        i > 5 -> "5보다 크다"
+        else -> "!!!"
+    }
+    println(result)
 }
