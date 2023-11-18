4e3a5d3364e4624def5b4fe3a5cacf2451622966
b1urrrr <b1urrr@naver.com>
2023-11-18 23:22:57 +0900

    [ADD/#1] 텍스트 컴포즈 및 Column 실습

--- modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
+++ modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
@@ -3,13 +3,16 @@ package com.ocomwan.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.ocomwan.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +25,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                    ) {
+                        Text("Hello")
+                        Text("World")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
