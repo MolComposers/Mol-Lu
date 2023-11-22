202744f24e5e46eb495a8685340e147d3cbadf08
b1urrrr <b1urrr@naver.com>
2023-11-22 21:50:19 +0900

    [ADD/#1] 이전 학습한 내용 함수 추가

--- modern-android-compose/section00/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
+++ modern-android-compose/section00/app/src/main/java/com/ocomwan/kotlinbasics/Main.kt
@@ -1,12 +1,62 @@
 package com.ocomwan.kotlinbasics
 
 import java.util.Scanner
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.random.Random
 
+const val num = 20 // Compile Time Constant
+
 fun main() {
-    practiceRandom()
-    practiceKeyboardInput()
-    practiceConditionalExpression()
+}
+
+private fun practicePrint() {
+    println("Hello World")
+    print("Hello World")
+}
+
+private fun practiceVariablesAndConstants() {
+    var i = 10
+    var name = "채연"
+    var point = 3.3
+
+    i = 20
+
+    val num = 20
+    // num = 30
+}
+
+private fun practiceTypeConversion() {
+    var i = 10
+    var l = 20L
+
+    l = i.toLong()
+    i = l.toInt()
+
+    var name = ""
+    name = i.toString()
+
+    val name2 = "10"
+    i = name2.toInt()
+}
+
+private fun practiceString() {
+    var name = "hello"
+    print(name.uppercase())
+    print(name.lowercase())
+    print(name[0])
+
+    name = "채연"
+    print("제 이름은 ${name}입니다.")
+    print("제 이름은 ${name + 10}입니다.")
+}
+
+private fun practiceMaxAndMin() {
+    val i = 10
+    val j = 20
+
+    print(max(i, j))
+    print(min(i, j))
 }
 
 private fun practiceRandom() {
