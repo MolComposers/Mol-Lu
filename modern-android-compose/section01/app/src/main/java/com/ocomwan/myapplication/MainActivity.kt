fa3406a2010958465a4496e2909cad24b7b5deff
b1urrrr <b1urrr@naver.com>
2023-11-30 20:56:36 +0900

    [ADD/#1] 컴포저블 함수화 및 프리뷰 실습

--- modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
+++ modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ocomwan.myapplication.ui.theme.MyApplicationTheme
 
@@ -20,20 +24,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(16.dp),
-                    ) {
-                        Text("Hello")
-                        Text("World")
-                    }
-                }
+                practiceColumn()
             }
         }
+    }
+
+    @Composable
+    fun practiceColumn() {
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Blue)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text("Hello")
+                Text("World")
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun defaultPreview() {
+        practiceColumn()
     }
 }
