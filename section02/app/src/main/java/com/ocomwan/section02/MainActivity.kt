706839fc15477dcdebad45ebad32a08175d44301
b1urrrr <b1urrr@naver.com>
2023-12-01 23:30:47 +0900

    [ADD/#2] 비만도 계산기 첫 번째 화면 작성

--- section02/app/src/main/java/com/ocomwan/section02/MainActivity.kt
+++ section02/app/src/main/java/com/ocomwan/section02/MainActivity.kt
@@ -1,29 +1,82 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.ocomwan.section02
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ocomwan.section02.ui.theme.Section02Theme
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Section02Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    val (height, setHeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+    val (weight, setWeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("비만도 계산기") },
+            )
+        },
+    ) { padding ->
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            OutlinedTextField(
+                value = height,
+                onValueChange = setHeight,
+                label = { Text("키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text("몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp),
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.End),
+            ) {
+                Text("결과")
             }
         }
     }
@@ -33,14 +86,6 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Section02Theme {
-        Greeting("Android")
-    }
 }
