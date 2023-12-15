5f0c1ea6acd8f9e988b4f5cdc3ad3ecd85d2e21a
b1urrrr <20190993@sungshin.ac.kr>
2023-12-15 23:22:53 +0900

    [ADD/#1] TextField, Button, Snackbar 실습

--- modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
+++ modern-android-compose/section01/app/src/main/java/com/ocomwan/myapplication/MainActivity.kt
@@ -1,3 +1,11 @@
+@file:OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class,
+    ExperimentalComposeUiApi::class,
+)
+
 package com.ocomwan.myapplication
 
 import android.os.Bundle
@@ -6,10 +14,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,32 +30,41 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ocomwan.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                PracticeImageCardState()
+                PracticeTextFieldButtonSnackBar()
             }
         }
     }
@@ -182,11 +201,52 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Composable
+    fun PracticeTextFieldButtonSnackBar() {
+        val (inputText, setInputText) = rememberSaveable {
+            mutableStateOf("")
+        }
+        val snackbarHostState = remember {
+            SnackbarHostState()
+        }
+        val coroutineScope = rememberCoroutineScope()
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White).clickable {
+                    keyboardController?.hide()
+                },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextField(
+                value = inputText,
+                onValueChange = setInputText,
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+            Button(onClick = {
+                keyboardController?.hide()
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(
+                        getString(
+                            R.string.section1_input_text,
+                            inputText,
+                        ),
+                    )
+                }
+            }) {
+                Text(stringResource(R.string.section1_click))
+            }
+        }
+    }
+
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         MyApplicationTheme {
-            PracticeImageCardState()
+            PracticeTextFieldButtonSnackBar()
         }
     }
 }
